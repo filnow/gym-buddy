636791e7cac9c7c6add146a2590c070884e7df2e
filnow <filnow3@st.amu.edu.pl>
2023-09-19 21:29:22 +0200

    adding exercise to list by button

--- src/components/SideCard.tsx
+++ src/components/SideCard.tsx
@@ -1,6 +1,6 @@
 import { ExpandLess, ExpandMore, StarBorder } from "@mui/icons-material";
 import { Collapse, List, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { categories } from '../constants/CategoryList';
 import ClearIcon from '@mui/icons-material/Clear';
 
@@ -12,27 +12,42 @@ export default function SideCard(
         id: number
     }
 ) {
+    
+    const exercise = categories.flatMap(category => category.data).find(exercise => exercise.id === id)?.name;
 
     const [open, setOpen] = useState(false);
+    const [exerciseList, setExerciseList] = useState<string[]>([]);
 
     const handleClick = () => {
         setOpen(!open);
     };
 
+    useEffect(() => {
+        if (exercise && !exerciseList.includes(exercise)) {
+            setExerciseList(prevExerciseList => [...prevExerciseList, exercise]);
+        }
+    }, [id]);
     
-    const exercise = categories.flatMap(category => category.data).find(exercise => exercise.id === id)?.name;
+    const handleDelete = (indexToDelete: number) => {
+        setExerciseList((prevExerciseList) =>
+          prevExerciseList.filter((_, index) => index !== indexToDelete)
+        );
+      };
 
 
-    console.log(exercise);
     return (
         <List>
-            <ListItemButton onClick={handleClick}>
-                <ListItemIcon>
-                    <ClearIcon />
-                </ListItemIcon>
-                <ListItemText primary={exercise} />
-                {open ? <ExpandLess /> : <ExpandMore />}
-            </ListItemButton>
+            {exerciseList.map((exerciseItem, index) => (
+                <ListItemButton key={index}>
+                    <ListItemIcon>
+                        <ClearIcon onClick={() => handleDelete(index)}/>
+                    </ListItemIcon>
+                    <ListItemText primary={exerciseItem} />
+                    <ListItemIcon onClick={handleClick}>
+                        {open ? <ExpandLess /> : <ExpandMore />}
+                    </ListItemIcon>
+                </ListItemButton>
+            ))}
             <Collapse in={open} timeout="auto" unmountOnExit>
                 <List component="div" disablePadding>
                     <ListItemButton sx={{ pl: 4 }}>
